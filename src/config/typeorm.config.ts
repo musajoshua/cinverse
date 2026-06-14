b1fe9b756a18f0459fefdbb6c601e48ebfe6c1f0
musajoshua <gidijosh@gmail.com>
2026-06-14 20:49:36 +0200

    feat: add TypeORM configuration for migration

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+config();
+
+const configService = new ConfigService();
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  port: +configService.get<string>('DB_PORT')!,
+  database: configService.get<string>('DB_NAME'),
+  synchronize: false,
+  entities: ['src/**/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+  migrationsRun: false,
+});
+
+export default AppDataSource;
