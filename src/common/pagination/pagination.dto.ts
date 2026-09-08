18661182b5e13dcbf7748ceed31dfdb9f436d69a
musajoshua <gidijosh@gmail.com>
2026-09-09 00:18:49 +0200

    feat: add Type transformation to limit and offset in PaginationDto

--- src/common/pagination/pagination.dto.ts
+++ src/common/pagination/pagination.dto.ts
@@ -1,11 +1,14 @@
+import { Type } from 'class-transformer';
 import { IsPositive, IsOptional } from 'class-validator';
 
 export class PaginationDto {
   @IsOptional()
   @IsPositive()
+  @Type(() => Number)
   limit?: number;
 
   @IsOptional()
   @IsPositive()
+  @Type(() => Number)
   offset?: number;
 }
